a714c616484499c604ee28339fb1f64ca1a44a68
Matt Hill <matthewhill2595@gmail.com>
2021-03-04 21:57:30 +0000

    Initial fix of code provided for the assignment.

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+public class Board {
+
+    private int board[][];
+
+    public Board(int height, int width){
+        board = new int[height][width];
+    }
+
+    public void reset(){
+
+    }
+
+}
--- src/MyConnectFour.java
+++ src/MyConnectFour.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class myconnectfour {
-
-    public static void main(String[] args){
-        new MyConnectFour();
-    }
+public class MyConnectFour {
 
     private BufferedReader input;
     private char[][] board;
@@ -19,17 +16,17 @@ public class myconnectfour {
     private void playGame(){
         System.out.println("Welcome to Connect 4");
         System.out.println("There are 2 players red and yellow");
-        System.out.println("Player 1 is Red, Player 2 is Yellow");
-        System.out.println("To play the game type in the number of the column you want to drop you counter in")
+        System.out.println("Player 1 is Red, Player 2 is Yellow"); // Is this correct?
+        System.out.println("To play the game type in the number of the column you want to drop you counter in");
         System.out.println("A player wins by connecting 4 counters in a row - vertically, horizontally or diagonally");
         System.out.println("");
         printBoard();
         boolean win = false;
         while(!win){
             // player 1
-            String userInput = getuserInput();
+            String userInput = getUserInput();
             int move = Integer.parseInt(userInput);
-            placeCounter('r',userInput);
+            placeCounter('r',move);
             boolean hasWon = false;
             int count = 0;
             // check horizontal
@@ -37,23 +34,24 @@ public class myconnectfour {
                 for(int j=0; j<board[i].length; j++){
                     if(board[i][j] == 'r'){
                         count = count + 1;
-                        if(count > 4){
+                        if(count >= 4){
                             hasWon = true;
                         }
                     }
                     else{
+                        // if not, reset the count
                         count = 0;
                     }
                 }
 
             }
-            // check vertical 
+            // check vertical
             count = 0;
             for(int i=0; i<board[0].length; i++){
                 for(int j=0; j<board.length; j++){
                     if(board[j][i] == 'r'){
                         count = count + 1;
-                        if(count > 4){
+                        if(count >= 4){
                             hasWon = true;
                         }
                     }
@@ -66,73 +64,77 @@ public class myconnectfour {
             printBoard();
             if(hasWon){
                 win = true;
+                System.out.println("Player one has won");
             }
-            else{
+            else {
                 //player 2
                 userInput = getUserInput();
                 move = Integer.parseInt(userInput);
-                placeCounter('y',move);
+                placeCounter('y', move);
                 hasWon = false;
                 count = 0;
                 // check horizontal
-                for(int i=0; i<board.length; i++){
-                    for(int j=0; j<board[i].length; j++){
-                        if(board[i][j] == 'y'){
+                for (int i = 0; i < board.length; i++) {
+                    for (int j = 0; j < board[i].length; j++) {
+                        if (board[i][j] == 'y') {
                             count = count + 1;
-                            if(count >= 4){
+                            if (count >= 4) {
                                 hasWon = true;
                             }
-                        }
-                        else{
-
+                        } else {
+                            count = 0;
                         }
                     }
-                    count = 0;
+
                 }
-                // check vertical 
+                // check vertical
                 count = 0;
-                for(int i=0; i<board[0].length; i++){
-                    for(int j=0; j<board.length; j++){
-                        if(board[j][i] == 'y'){
+                for (int i = 0; i < board[0].length; i++) {
+                    for (int j = 0; j < board.length; j++) {
+                        if (board[j][i] == 'y') {
                             count = count + 1;
-                            if(count >= 4){
+                            if (count >= 4) {
                                 hasWon = true;
                             }
-                        }
-                        else{
-
+                        } else {
+                            count = 0;
                         }
                     }
-                    count = 0;
+
                 }
-                printBoard()
-                if(hasWon){
+                printBoard();
+                if (hasWon) {
+                    System.out.println("Player 2 won!");
                     win = true;
                 }
             }
-            System.out.println("You Have Won!!!");
         }
-
+        System.out.println("You Have Won!!!");
     }
 
     private String getUserInput(){
         String toReturn = null;
         try{
-            String toReturn = input.readLine()
+            toReturn = input.readLine();
         }
         catch(Exception e){
-
+            System.out.println("Please enter a valid number: ");
+            getUserInput();
         }
         return toReturn;
     }
 
+    private void printBoard2(){
+        System.out.println(Arrays.deepToString(board));
+    }
+
     private void printBoard(){
-        for(int i=0; i<board.length-1; i++);{
-            for(int j=0; j<baord[i].length-1; j++){
-                if(board[j][i] == 'r'){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[i].length; j++){
+                if(board[i][j] == 'r'){
                     System.out.print("| r ");
                 }
-                else if(board[j][i] == 'y'){
+                else if(board[i][j] == 'y'){
                     System.out.print("| y ");
                 }
                 else{
@@ -147,14 +149,14 @@ public class myconnectfour {
     private void placeCounter(char player, int position){
         boolean placed = false;
         if(player == 'r'){
-            for(int i=board.length-1; i>=0; i++){
+            for(int i=board.length-1; i>=0; i--){
                 if(!placed){
-                    if(board[i][position] == 'y'){
+                    if(board[i][position-1] == 'y'){
                         // skip
                     }
-                    else if(board[i][position] != 'r'){
-                        board[i][position] = 'r';
-                        placed = ture;
+                    else if(board[i][position-1] != 'r'){
+                        board[i][position-1] = 'r';
+                        placed = true;
                     }
                 }
             }
@@ -162,11 +164,11 @@ public class myconnectfour {
         else{
             for(int i=board.length-1; i>=0; i--){
                 if(!placed){
-                    if(board[i][position-1] = 'r'){
+                    if(board[i][position-1] == 'r'){
                         // skip
                     }
                     else if(board[i][position-1] != 'y'){
-                        board[i][position-1] = 'y'
+                        board[i][position-1] = 'y';
                         placed = true;
                     }
                 }
